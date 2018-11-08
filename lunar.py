61f88c60bcbc55eb6eba570ec220963898b116a2
rnharmening <robin.harmening@student.uni-tuebingen.de>
2018-11-08 07:34:44 +0100

    multiply working

--- lunar.py
+++ lunar.py
@@ -8,21 +8,17 @@ class Lunar:
     def __add__(self, other):
         a, b = self.value, other.value
         a, b = (str(a), str(b)) if a > b else (str(b), str(a))
-        # b = ''.join("0" for _ in range(len(a)-len(b))) + b
 
         c = a[:len(a)-len(b)] + ''.join(x if x > y else y for x, y in zip(a[::-1], b[::-1]))[::-1]
 
         return Lunar(int(c))
 
-    # TODO not done yet
     def __mul__(self, other):
         a, b = self.value, other.value
         a, b = (str(a), str(b)) if a > b else (str(b), str(a))
         res = Lunar(0)
         for i, b_ in enumerate(str(b[::-1])):
-            c = Lunar(int(a[:len(a)-len(b)]
-                      + ''.join(x if x < y else y for x, y in zip(a[::-1], b_[::-1]))[::-1]
-                      + ''.join("0" for _ in range(i))))
+            c = Lunar(int(''.join(x if x < b_ else b_ for x in a) + ''.join("0" for _ in range(i))))
             res += c
 
         return res
@@ -31,8 +27,9 @@ class Lunar:
         return str(self.value)
 
 
+if __name__ == '__main__':
 
-x, y = Lunar(115), Lunar(115)
+    x, y = Lunar(9), Lunar(158)
 
-print(x + y)
-print(x * y)
+    print(x + y)
+    print(x * y)
